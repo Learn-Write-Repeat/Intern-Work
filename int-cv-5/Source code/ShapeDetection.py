285b4ed0a31a78628f756cddcbc38e1bcb0090bc
sreelakshmig009 <sreelakshmig009@gmail.com>
2021-07-05 18:40:02 +0530

    Update ShapeDetection.py

--- int-cv-5/Source code/ShapeDetection.py
+++ int-cv-5/Source code/ShapeDetection.py
@@ -1,42 +1,45 @@
 import cv2
+def getContours(img):
+    contours , heirarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area >100:
+            cv2.drawContours(imgContours,cnt,-1,(0,0,0),2)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            objectCor = len(approx)
+            x,y,w,h = cv2.boundingRect(approx)
 
-class ShapeDetector:
-    def __init__(self):
-        pass
+            if objectCor == 3:
+                objectType = "Triangle"
 
-    def detect(self, c):
-        # initialize the shape name and approximate the contour
-        shape = "unidentified"
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+            elif objectCor == 4:
+                assRatio = w/float(h)
+                if assRatio >0.95 and assRatio<1.05:
+                    objectType = "Square"
+                else:
+                    objectType = "Rectangle"
+            elif objectCor == 5:
+                objectType = "Pentagon"
+            elif objectCor == 6 :
+                objectType = "Hexagon"
+            elif objectCor > 6:
+                objectType = "Circle"
+            else:
+                objectType = "None"
 
-        # if the shape is a triangle, it will have 3 vertices
-        if len(approx) == 3:
-            shape = "Triangle"
-            
-        elif len(approx) == 10:
-            shape = "Star"
+            cv2.rectangle(imgContours,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContours,objectType,(x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
 
+path = "path/to/image"
+img = cv2.imread(path)
+imgContours = img.copy()
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+imgCanny = cv2.Canny(imgBlur,50,50)
+getContours(imgCanny)
 
-        # if the shape has 4 vertices, it is either a square or
-        # a rectangle
-        elif len(approx) == 4:
-            # compute the bounding box of the contour and use the
-            # bounding box to compute the aspect ratio
-            (x, y, w, h) = cv2.boundingRect(approx)
-            ar = w / float(h)
+cv2.imshow("Shapes",img)
+cv2.imshow("output",imgContours)
 
-            # a square will have an aspect ratio that is approximately
-            # equal to one, otherwise, the shape is a rectangle
-            shape = "Square" if ar >= 0.95 and ar <= 1.05 else "Rectangle"
-
-        # if the shape is a pentagon, it will have 5 vertices
-        elif len(approx) == 5:
-            shape = "pentagon"
-
-        # otherwise, we assume the shape is a circle
-        else:
-            shape = "circle"
-
-        # return the name of the shape
-        return shape
+cv2.waitKey(0)
